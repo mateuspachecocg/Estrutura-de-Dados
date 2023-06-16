5794fd0537c0eba1a6f5ed5be844988a0cbae008
Mateus Pacheco <mateuspacheco@alu.ufc.br>
2023-06-15 23:45:08 -0300

    Adding Function to Dot H file

--- trabalho_2_ed/q2/sorting_algorithms.h
+++ trabalho_2_ed/q2/sorting_algorithms.h
@@ -1,10 +1,12 @@
-
 /* Sorting Algorithms Functions */
 void bubble_sort(int* array, int n);
 void insertion_sort(int* array, int n);
 void quick_sort(int* array, int start, int end);
 int partition(int* array, int start, int end);
-void merge(int* array, int l, int m, int r);
+void merge(int arr[], int l, int m, int r);
+void mergesort(int arr[], int l, int r);
+void heapify(int arr[], int n, int i);
+void heapsort(int arr[], int n);
 
 /* Utility Functions */
 void array_print(int* array, int n);
